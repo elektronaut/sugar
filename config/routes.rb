39ad2fdffc35c3333bbe441a4da5dbbb917455d4
Inge Jørgensen <inge@elektronaut.no>
2008-06-27 19:01:43 +0000

    Default route

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ ActionController::Routing::Routes.draw do |map|
     # Install the default routes as the lowest priority.
     map.connect ':controller/:action/:id'
     map.connect ':controller/:action/:id.:format'
+
+    map.root :controller => 'discussions', :action => 'index'
 end
