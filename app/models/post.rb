1926fb8bd62475ae3597e928bab727e480db9e28
Inge Jørgensen <inge@elektronaut.no>
2008-06-29 06:46:37 +0000

    New stylez

--- app/models/post.rb
+++ app/models/post.rb
@@ -61,6 +61,17 @@ class Post < ActiveRecord::Base
     end
 
     def body_html
+        string = self.body
+        string = string.strip
+
+        # Autolink URLs
+        string.gsub!(/(^|\s)((ftp|https?):\/\/[^\s]+)\b/){ "#{$1}<a href=\"#{$2}\">#{$2}</a>" }
+        
+        # Replace line breaks
+        string.gsub!(/\r?\n/,'<br />')
+
+        return string
+
         unless body_html?
             self.update_attribute(:body_html, PostParser.parse(self.body.dup))
         end
