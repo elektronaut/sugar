b3677deb95c719efe38da8394b1c75363442ee17
Inge Jørgensen <inge@elektronaut.no>
2008-06-30 12:22:54 +0000

    301 redirects

--- app/controllers/vanilla_controller.rb
+++ app/controllers/vanilla_controller.rb
@@ -4,6 +4,7 @@ class VanillaController < ApplicationController
     # http://butt3rscotch.org/vanilla/?page=2
     # http://butt3rscotch.org/vanilla/?CategoryID=11
     def discussions
+        headers["Status"] = "301 Moved Permanently"
         if params[:CategoryID]
             redirect_to category_url(params[:CategoryID])
         else
@@ -13,11 +14,13 @@ class VanillaController < ApplicationController
 
     # http://butt3rscotch.org/vanilla/comments.php?DiscussionID=13892&page=6
     def discussion
+        headers["Status"] = "301 Moved Permanently"
         redirect_to paged_discussion_url(:id => params[:DiscussionID], :page => params[:page])
     end
 
     # http://butt3rscotch.org/vanilla/account.php?u=4
     def user
+        headers["Status"] = "301 Moved Permanently"
         redirect_to user_url(:id => params[:u])
     end
 
