9e70a0c475f396245e74c5581baff2f20db64f7e
Inge Jørgensen <inge@elektronaut.no>
2008-08-28 03:02:38 +0000

    Added indexes to cached trusted status

--- db/migrate/20080828010822_trusted_post_fields.rb
+++ db/migrate/20080828010822_trusted_post_fields.rb
@@ -4,9 +4,13 @@ class TrustedPostFields < ActiveRecord::Migration
         Discussion.find(:all, :conditions => 'trusted = 1').each do |discussion|
             Post.update_all("trusted = 1", "discussion_id = #{discussion.id}")
         end
+        add_index :posts, :trusted, :name => 'trusted_index'
+        add_index :discussions, :trusted, :name => 'trusted_index'
     end
 
     def self.down
+        remove_index :posts, :name => 'trusted_index'
+        remove_index :discussions, :name => 'trusted_index'
         remove_column :posts, :trusted
     end
 end
