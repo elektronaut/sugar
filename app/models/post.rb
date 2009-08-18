1fa529e35f8a2d04ecaccf7c7d5707d540071e27
Inge Jørgensen <inge@manualdesign.no>
2009-08-18 04:07:01 +0200

    Malicious script tags, event handlers and attributes on embedded objects are now filtered (and corrected) using Hpricot

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,6 @@ class Post < ActiveRecord::Base
 
 	validate do |post|
 		post.trusted = post.discussion.trusted if post.discussion
-		post.errors.add(:body, "script tags aren't allowed") if post.body =~ /<script/
 		post.edited_at ||= Time.now
 	end
 
--- lib/post_parser.rb
+++ lib/post_parser.rb
@@ -1,5 +1,5 @@
 require 'hpricot'
-require 'uv'
+#require 'uv'
 
 module PostParser
     def PostParser.parse(string)
@@ -8,14 +8,59 @@ module PostParser
 		# Autolink URLs
 		string.gsub!(/(^|\s)((ftp|https?):\/\/[^\s]+)\b/){ "#{$1}<a href=\"#{$2}\">#{$2}</a>" }
         
-        # Youtube videos
-        # <video type="youtube">td6m3OhO5zE</video>
-        #string.gsub(/<video type="youtube">[\w\d]+<\/video>/){}
-
 		doc = Hpricot(string)
-		doc.search('code') do |codeblock|
-			codeblock.swap( Uv.parse(codeblock.inner_html, "xhtml", 'javascript', true, 'twilight') )
+		
+		# Delete <script> tags
+		(doc/"script").remove
+		
+		# Filter malicious attributes on all elements
+		doc.search("*").select{ |e| e.elem? }.each do |elem|
+			if elem.raw_attributes
+				elem.raw_attributes.each do |name, value|
+					# Strip out event handlers
+					elem.raw_attributes.delete(name) if name.downcase =~ /^on/
+					# Change allowScriptAccess to sameDomain
+					elem.raw_attributes[name] = 'sameDomain' if name.downcase =~ /^allowscriptaccess/
+				end
+			end
 		end
+		
+		# Enforce correct allowScriptAccess on embed tags
+		doc.search("embed").each do |elem| 
+			if elem.raw_attributes
+				if !elem.raw_attributes.keys.map(&:downcase).include?('allowscriptaccess')
+					elem.raw_attributes['allowScriptAccess'] = 'sameDomain'
+				end
+			else
+				elem.raw_attributes = {'allowScriptAccess' => 'sameDomain'}
+			end
+		end
+		
+		# Filter param tags for malicious values
+		doc.search("param").each do |elem|
+			if elem.raw_attributes
+				elem.raw_attributes.each do |name, value|
+					elem.raw_attributes = {'name' => 'allowScriptAccess', 'value' => 'sameDomain'} if name.downcase == 'name' && value.downcase == 'allowscriptaccess'
+				end
+			end
+		end
+		
+		# Make sure there's a <param name="allowScriptAccess" value="sameDomain"> in object tags
+		doc.search("object").each do |elem|
+			param_attributes = elem.search('>param').map do |subelem| 
+				subelem.attributes ? subelem.attributes.map{|k,v| (k.downcase == 'name') ? v.downcase : nil }.compact : []
+			end
+			unless param_attributes.flatten.include?('allowscriptaccess')
+				elem.inner_html += '<param name="allowScriptAccess" value="sameDomain" />'
+			end
+		end
+
+		# Parse <code> blocks
+		# doc.search('code') do |codeblock|
+		# 	codeblock.swap( Uv.parse(codeblock.inner_html, "xhtml", 'javascript', true, 'twilight') )
+		# end
+
+		# ..and convert back to HTML again
 		string = doc.to_html
 
         # Replace line breaks
