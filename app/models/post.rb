884b9fedd9a88d2ea0e7b283226a6bf154753b10
Inge Jørgensen <inge@manualdesign.no>
2009-08-18 05:56:16 +0200

    Cache parsed posts in dev mode too

--- app/models/post.rb
+++ app/models/post.rb
@@ -88,9 +88,6 @@ class Post < ActiveRecord::Base
 	end
 
 	def body_html
-		if RAILS_ENV == 'development'
-			return PostParser.parse(self.body.dup)
-		end
 		unless body_html?
 			self.update_attribute(:body_html, PostParser.parse(self.body.dup))
 		end
