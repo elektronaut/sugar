1f5352b290c2544cdca4278d538d2569e76d99c5
Inge Jørgensen <inge@manualdesign.no>
2010-01-14 23:13:59 +0100

    New login form

--- public/javascripts/sugar/sugar.js
+++ public/javascripts/sugar/sugar.js
@@ -2,6 +2,30 @@ var Sugar = {
 	Configuration: {},
 	onLoadedPosts: {},
 	Initializers: {
+		
+		loginForm : function() {
+			$('body.login #login').each(function(){
+				var container = this;
+				
+				if($.cookie('login_method') == 'openid') {
+					$(container).find('.username_and_password.form').hide();
+				} else {
+					$(container).find('.openid.form').hide();
+				}
+				
+				$(container).find('.openid_toggle').click(function(){
+					$(container).find('.username_and_password.form').hide();
+					$(container).find('.openid.form').show();
+					$.cookie('login_method', 'openid', {expires: 365})
+				});
+
+				$(container).find('.username_and_password_toggle').click(function(){
+					$(container).find('.username_and_password.form').show();
+					$(container).find('.openid.form').hide();
+					$.cookie('login_method', null, {expires: 365})
+				});
+			});
+		},
 
 		richText : function() {
 			jQuery('textarea.rich').each(function(){
