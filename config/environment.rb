fe85f5033d4e9832dd5a156975c7545b85ff0b7c
Inge Jørgensen <inge@manualdesign.no>
2010-09-22 22:55:15 +0200

    Use Bundler for managing dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -27,9 +27,9 @@ Rails::Initializer.run do |config|
 	# config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
 	# config.gem "aws-s3", :lib => "aws/s3"
 
-	config.gem 'ruby-openid', :lib => 'openid', :version => '2.1.6'
-	config.gem 'hpricot'
-	config.gem 'ultraviolet', :lib => 'uv'
+	#config.gem 'ruby-openid', :lib => 'openid', :version => '2.1.6'
+	#config.gem 'hpricot'
+	#config.gem 'ultraviolet', :lib => 'uv'
 
 	# Only load the plugins named here, in the order given. By default, all plugins 
 	# in vendor/plugins are loaded in alphabetical order.
