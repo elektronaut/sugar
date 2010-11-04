6df154a918508f4e81d41241ac1560328d99c73b
Inge Jørgensen <inge@elektronaut.no>
2010-11-04 17:38:42 +0100

    Expire sessions after 3 years

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Sugar::Application.config.session_store :cookie_store, :key => (Sugar.config(:session_key) rescue '_sugar_session')
+Sugar::Application.config.session_store(
+	:cookie_store, 
+	:key          => (Sugar.config(:session_key) rescue '_sugar_session'), 
+	:expire_after => 3.years
+)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
