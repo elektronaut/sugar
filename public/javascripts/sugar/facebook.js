7b9045b2ebf2ca2fa059663d0778fd493b29c1f5
Inge Jørgensen <inge@manualdesign.no>
2010-12-26 18:34:19 +0100

    Made the client-side Facebook code more readable

--- public/javascripts/sugar/facebook.js
+++ public/javascripts/sugar/facebook.js
@@ -1,123 +1,154 @@
 /*jslint browser: true, devel: true, onevar: false, regexp: false, immed: false*/
 /*global window: false, jQuery: false, $: false, Sugar: false, FB: false*/
 
-(function ($S) {
-	
-	$($S).bind('ready', function () {
-		if (this.Configuration.FacebookAppId) {
-			this.Facebook.init();
-		}
-	});
-	
-	$S.Facebook = {
-		appId: false,
-		init: function () {
-			var facebook_lib = this;
-			this.appId = $S.Configuration.FacebookAppId;
+// Listens for a ready event from the framework, trigger init()
+// if the application ID is configured. 
+$(Sugar).bind('ready', function () {
+	if (this.Configuration.FacebookAppId) {
+		this.Facebook.init();
+	}
+});
 
-			$(this).bind('ready', function () {
-				this.applyFunctionality();
+Sugar.Facebook = {
+
+	// The application ID.
+	appId: false,
+
+	// Initializer
+	init: function () {
+		var facebook_lib = this;
+		this.appId = Sugar.Configuration.FacebookAppId;
+
+		// Listens for a ready event on the Facebook object, which will be triggered
+		// when everything is done loading.
+		$(this).bind('ready', function () {
+			this.applyFunctionality();
+		});
+
+		// Sets up the initialize function which Facebook calls when the script is embedded.
+		window.fbAsyncInit = function () {
+
+			// Let's get started.
+			FB.init({
+				appId  : Sugar.Facebook.appId,
+				status : true, // check login status
+				cookie : true, // enable cookies to allow the server to access the session
+				xfbml  : true  // parse XFBML
 			});
 
-			// Connect to Facebook asynchronously, jQuery style
-			$('body').append('<div id="fb-root" />');
-			window.fbAsyncInit = function () {
-				// Initialize Facebook
-				FB.init({
-					appId  : $S.Facebook.appId,
-					status : true, // check login status
-					cookie : true, // enable cookies to allow the server to access the session
-					xfbml  : true  // parse XFBML
-				});
-				// Bind events
-				FB.Event.subscribe('auth.sessionChange', function (response) {
-					if ($.cookie('facebook_logout') === 'true') {
-						FB.logout(function () {});
-						$.cookie('facebook_logout', null, {path: '/'});
+			// Subscribe to the auth.sessionChange event, which is called whenever the
+			// state over at Facebook changes.
+			FB.Event.subscribe('auth.sessionChange', function (response) {
+
+				// Did the backend tell us to log out? If so, terminate the session. 
+				// This has the side effect of logging us out of Facebook, which is
+				// silly, but that's how it's supposed to work. Oh well.
+				if ($.cookie('facebook_logout') === 'true') {
+					FB.logout(function () {});
+					$.cookie('facebook_logout', null, {path: '/'});
+
+				// If not, let's emit some nice jQuery events so we won't have to deal
+				// with the Facebook API directly. 
+				} else {
+					$(facebook_lib).trigger('auth.sessionChange');
+					if (response.session) {
+						$(facebook_lib).trigger('auth.login');
 					} else {
-						$(facebook_lib).trigger('auth.sessionChange');
-						if (response.session) {
-							$(facebook_lib).trigger('auth.login');
-						} else {
-							$(facebook_lib).trigger('auth.logout');
-						}
+						$(facebook_lib).trigger('auth.logout');
 					}
-				});
-				$(facebook_lib).trigger('ready');
-			};
-			var e = document.createElement('script');
-			e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-			e.async = true;
-			document.getElementById('fb-root').appendChild(e);
-		},
-
-		applyFunctionality: function () {
-			// Format buttons
-			$('.facebook_login').addClass('fb_button fb_button_large').wrapInner('<span class="fb_button_text" />');
-
-			// Connect under Services
-			$('.edit_user_profile .facebook_login').click(function () {
-				$S.Facebook.loginAndRedirect('/users/connect_facebook');
-				return false;
-			});
-
-			// Login handler
-			$('#login .facebook_login').click(function () {
-				$S.Facebook.loginAndRedirect('/');
-				return false;
+				}
 			});
 			
-			// Signing up
-			var doSignup = function () {
-				$('#facebook .signup').hide();
-				FB.getLoginStatus(function (response) {
+			// That's it, we're ready.
+			$(facebook_lib).trigger('ready');
+		};
+
+		// All the callbacks have been set up, let's embed the Facebook scripts.
+		$('body').append('<div id="fb-root" />');
+		var e = document.createElement('script');
+		e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+		e.async = true;
+		document.getElementById('fb-root').appendChild(e);
+	},
+
+	// Checks Facebook login status and redirects to the specified URL if there is a session. 
+	// If we don't have a Facebook session, a login is prompted, optionally asking for permissions.
+	// If the login succeeds, the redirect is performed. 
+	loginAndRedirect: function (redirectUrl) {
+		FB.getLoginStatus(function (response) {
+			if (response.session) {
+				document.location = redirectUrl;
+			} else {
+				FB.login(function (response) {
 					if (response.session) {
-						$('#facebook .connect').hide();
-						$('#facebook .signup').show();
-						FB.api('/me', function (response) {
-							$('#facebook .login_status').html('You are logged into Facebook as <strong>' + response.name + '</strong>.');
-							if (!$('#facebook #user_username').val()) {
-								$('#facebook #user_username').val(response.name);
-							}
-							if (!$('#facebook #user_realname').val()) {
-								$('#facebook #user_realname').val(response.name);
-							}
-						});
+						document.location = redirectUrl;
 					}
-				});
-			};
-			$('.signup #facebook').each(function () {
-				doSignup();
-			});
-			$('.signup .facebook_login').click(function () {
-				FB.getLoginStatus(function (response) {
-					if (response.session) {
-						doSignup();
-					} else {
-						FB.login(function (response) {
-							if (response.session) {
-								doSignup();
-							}
-						}, {perms: 'email,user_likes,read_friendlists,offline_access'});
-					}
-				});
-				return false;
-			});
-		},
+				}, {perms: 'user_likes,read_friendlists,offline_access'});
+			}
+		});
+	},
+
+	applyFunctionality: function () {
+
+		// Add some stuff to the login buttons so that Facebook can format it properly. 
+		$('.facebook_login').addClass('fb_button fb_button_large').wrapInner('<span class="fb_button_text" />');
+
+		// This is the button on the login screen, which should just redirect to the root path
+		// if we can get a Facebook session. The backend handles the rest.
+		$('#login .facebook_login').click(function () {
+			Sugar.Facebook.loginAndRedirect('/');
+			return false;
+		});
 		
-		loginAndRedirect: function (redirectUrl) {
+		// This is the button that will let you connect your Facebook account while
+		// editing your profile.
+		$('.edit_user_profile .facebook_login').click(function () {
+			Sugar.Facebook.loginAndRedirect('/users/connect_facebook');
+			return false;
+		});
+
+		// Handles signing up via Facebook. This can be done in a few ways,
+		// so let's wrap it up in a function.
+		var doSignup = function () {
+			$('#facebook .signup').hide();
 			FB.getLoginStatus(function (response) {
 				if (response.session) {
-					document.location = redirectUrl;
+					$('#facebook .connect').hide();
+					$('#facebook .signup').show();
+					FB.api('/me', function (response) {
+						$('#facebook .login_status').html('You are logged into Facebook as <strong>' + response.name + '</strong>.');
+						if (!$('#facebook #user_username').val()) {
+							$('#facebook #user_username').val(response.name);
+						}
+						if (!$('#facebook #user_realname').val()) {
+							$('#facebook #user_realname').val(response.name);
+						}
+					});
+				}
+			});
+		};
+
+		// Perform signup if .signup #facebook exists in the DOM.
+		$('.signup #facebook').each(function () {
+			doSignup();
+		});
+
+		// This is the Facebook login button on the signup form. Clicking it
+		// asks for email in addition to the regular permissions, which is nice
+		// to have when creating a new user.
+		$('.signup .facebook_login').click(function () {
+			FB.getLoginStatus(function (response) {
+				if (response.session) {
+					doSignup();
 				} else {
 					FB.login(function (response) {
 						if (response.session) {
-							document.location = redirectUrl;
+							doSignup();
 						}
-					}, {perms: 'user_likes,read_friendlists,offline_access'});
+					}, {perms: 'email,user_likes,read_friendlists,offline_access'});
 				}
 			});
-		}
-	};
-
-})(Sugar);
+			return false;
+		});
+	}
+};
