8fe213d76016b3e6e2e48eca4ee72325e6d82c47
Inge Jørgensen <inge@manualdesign.no>
2011-09-28 05:12:50 +0200

    Updated environment config for Rails 3.1, got rid of some crap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,37 +1,17 @@
 # encoding: utf-8
 
 Sugar::Application.configure do
-	# Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb
 
-	# The production environment is meant for finished, "live" apps.
-	# Code is not reloaded between requests
-	config.cache_classes = true
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-	# Full error reports are disabled and caching is turned on
-	config.consider_all_requests_local       = false
-	config.action_controller.perform_caching = true
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-	# Specifies the header that your server uses for sending files
-	config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
-	# For nginx:
-	# config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-	# If you have no front-end server that supports something like X-Sendfile,
-	# just comment this out and Rails will serve the files
-
-	# See everything in the log (default is :info)
-	# config.log_level = :debug
-
-	# Use a different logger for distributed setups
-	# config.logger = SyslogLogger.new
-
-	# Use a different cache store in production
-	# config.cache_store = :mem_cache_store
-
-	# Disable Rails's static asset server
-	# In production, Apache or nginx will already do this
-	config.serve_static_assets = false
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -39,21 +19,40 @@ Sugar::Application.configure do
   # Specify the default JavaScript compressor
   config.assets.js_compressor  = :uglifier
 
-	# Enable serving of images, stylesheets, and javascripts from an asset server
-	# config.action_controller.asset_host = "http://assets.example.com"
+  # Specifies the header that your server uses for sending files
+  # (comment out if your front-end server doesn't support this)
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
 
-	# Disable delivery errors, bad email addresses will be ignored
-	# config.action_mailer.raise_delivery_errors = false
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
 
-	# Enable threaded mode
-	# config.threadsafe!
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
 
-	# Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-	# the I18n.default_locale when a translation can not be found)
-	config.i18n.fallbacks = true
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
 
-	# Send deprecation notices to registered listeners
-	config.active_support.deprecation = :notify
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
 	
 	# Store all page caches in a subfolder of public
 	config.action_controller.page_cache_directory = File.join(File.dirname(__FILE__), '../../public/cache')
