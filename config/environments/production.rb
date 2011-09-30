cd70ab487d8045192060c0f96948a4851cad67e9
Inge Jørgensen <inge@manualdesign.no>
2011-09-30 02:03:31 +0200

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Sugar::Application.configure do
   config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
