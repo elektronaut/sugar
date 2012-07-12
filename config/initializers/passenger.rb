9703fd07b0023cd698df01434feb283a91accb3a
Inge Jørgensen <inge@manualdesign.no>
2012-07-13 01:54:44 +0200

    Reconnect to Redis on Passenger fork

---
+++ config/initializers/passenger.rb
@@ -0,0 +1,13 @@
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      # Re-establish redis connection
+      require 'redis'
+
+      # The important two lines
+      Sugar.redis.client.disconnect
+      Sugar.redis = Redis.new
+    end
+  end
+end
