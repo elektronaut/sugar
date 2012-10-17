fda70bc14ce7996774fa1257ca48108d8cf89293
Inge Jørgensen <inge@manualdesign.no>
2012-10-18 00:53:57 +0200

    Cleaned up the Conversation spec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -4,37 +4,35 @@ describe Conversation do
   it { should have_many :conversation_relationships }
   it { should have_many :participants }
 
-  before { @conversation = create(:conversation) }
+  let(:conversation) { create(:conversation) }
+  let(:user) { create(:user) }
 
   it 'inherits from Exchange' do
-    @conversation.should be_kind_of(Exchange)
+    conversation.should be_kind_of(Exchange)
   end
 
   it 'has the poster as a participant' do
-    @conversation.participants.should include(@conversation.poster)
+    conversation.participants.should include(conversation.poster)
   end
 
   it 'can add participants' do
-    user = create(:user)
-    @conversation.add_participant(user)
-    @conversation.participants.count.should eq(2)
-    @conversation.participants.should include(user)
+    conversation.add_participant(user)
+    conversation.participants.count.should eq(2)
+    conversation.participants.should include(user)
   end
 
   it 'can remove participants' do
-    user = create(:user)
-    @conversation.add_participant(user)
-    @conversation.remove_participant(user)
-    @conversation.participants.count.should eq(1)
-    @conversation.participants.should_not include(user)
+    conversation.add_participant(user)
+    conversation.remove_participant(user)
+    conversation.participants.count.should eq(1)
+    conversation.participants.should_not include(user)
   end
 
   it 'cannot remove the last participant' do
-    user = create(:user)
-    @conversation.add_participant(user)
-    @conversation.remove_participant(@conversation.poster)
+    conversation.add_participant(user)
+    conversation.remove_participant(conversation.poster)
     expect {
-      @conversation.remove_participant(user)
+      conversation.remove_participant(user)
     }.to raise_exception(Sugar::Exceptions::RemoveParticipantError)
   end
 
