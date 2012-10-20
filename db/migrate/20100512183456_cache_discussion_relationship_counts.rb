7aded2e62fa498da32764d39a7467b66344ebc2b
Inge Jørgensen <inge@manualdesign.no>
2012-10-20 03:39:37 +0200

    Migrations fixed for PostgreSQL

--- db/migrate/20100512183456_cache_discussion_relationship_counts.rb
+++ db/migrate/20100512183456_cache_discussion_relationship_counts.rb
@@ -3,10 +3,15 @@ class CacheDiscussionRelationshipCounts < ActiveRecord::Migration
     [:participated_count, :favorites_count, :following_count].each do |c|
       add_column :users, c, :integer, :null => false, :default => 0
     end
-    execute "UPDATE users SET
-      following_count    = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND following = 1),
-      favorites_count    = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND favorite = 1),
-      participated_count = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND participated = 1)"
+    User.update_all(
+      ["following_count = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND following = ?)", true]
+    )
+    User.update_all(
+      ["favorites_count = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND favorite = ?)", true]
+    )
+    User.update_all(
+      ["participated_count = (SELECT COUNT(*) FROM discussion_relationships WHERE discussion_relationships.user_id = users.id AND participated = ?)", true]
+    )
   end
 
   def self.down
