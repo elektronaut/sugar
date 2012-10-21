fc1b684fbde3d0ca250083e1bba1e398c62b56bf
Inge Jørgensen <inge@manualdesign.no>
2012-10-22 00:44:29 +0200

    Invite specs

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -5,13 +5,12 @@ require 'digest/sha1'
 class Invite < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :email, :user_id
-  
+
   attr_accessor :used
 
   DEFAULT_EXPIRATION = 14.days
 
   validate do |invite|
-    invite.token ||= Invite.unique_token
     if User.exists?(:email => invite.email)
       invite.errors.add(:email, 'is already registered!')
     end
@@ -19,18 +18,19 @@ class Invite < ActiveRecord::Base
       invite.errors.add(:email, 'has already been invited!')
     end
   end
-  
+
   before_create do |invite|
+    invite.token ||= Invite.unique_token
     invite.expires_at ||= Time.now + Invite.expiration_time
     if invite.valid?
       invite.user.revoke_invite!
     end
   end
-  
+
   before_destroy do |invite|
     invite.user.grant_invite! unless invite.used
   end
-  
+
   class << self
     # Makes a unique random token.
     def unique_token
@@ -38,22 +38,22 @@ class Invite < ActiveRecord::Base
       token = Digest::SHA1.hexdigest(rand(65535).to_s + Time.now.to_s) until token && !self.exists?(:token => token)
       token
     end
-    
+
     # Gets the default expiration time.
     def expiration_time
       DEFAULT_EXPIRATION
     end
-    
+
     # Finds valid invites
     def find_active
       self.find(:all, :conditions => ['expires_at >= ?', Time.now], :order => 'created_at DESC', :include => [:user])
     end
-    
+
     # Finds expired invites
     def find_expired
       self.find(:all, :conditions => ['expires_at < ?', Time.now], :order => 'created_at DESC')
     end
-    
+
     # Deletes expired invites
     def destroy_expired!
       self.find_expired.each do |invite|
@@ -61,12 +61,12 @@ class Invite < ActiveRecord::Base
       end
     end
   end
-  
+
   # Has this invite expired?
   def expired?
     (Time.now <= self.expires_at) ? false : true
   end
-  
+
   # Expire this invite
   def expire!
     self.used = true
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,95 +1,127 @@
 require 'spec_helper'
 
 describe Invite do
+
+  let(:invite)         { create(:invite) }
+  let(:expired_invite) { create(:invite, expires_at: 2.days.ago) }
+  let(:user)           { create(:user) }
+
   it { should belong_to(:user) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:user_id) }
-  
-  describe '#create' do
-    before do
-      @invite = create(:invite)
+
+  describe "email validation" do
+
+    context "when already registered" do
+      subject { build(:invite, email: user.email) }
+      it { should_not be_valid }
+      it { should have(1).errors_on(:email) }
     end
 
-    it 'should have an expiration date 14 days into the future' do
-      @invite.expires_at.should be_within(30).of(Time.now + 14.days)
+    context "when already invited" do
+      subject { build(:invite, email: invite.email) }
+      it { should_not be_valid }
+      it { should have(1).errors_on(:email) }
     end
 
-    it "verifies that the user isn't registered" do
-      user = create(:user, :email => 'foo@bar.com')
-      invite = build(:invite, :email => 'foo@bar.com')
-      invite.valid?.should be_false
-      invite.should have(1).errors_on(:email)
-    end
-    
-    it "verifies that the email isn't already invited" do
-      invite = build(:invite, :email => @invite.email)
-      invite.valid?.should be_false
-      invite.should have(1).errors_on(:email)
-    end
-    
-    it 'revokes an invite from the inviter' do
-      inviter = create(:user, :available_invites => 5)
-      expect { create(:invite, :user => inviter) }.to change{inviter.available_invites}.by(-1)
-    end
-    
-    it 'generates a token of at least 40 characters' do
-      @invite.token?.should be_true
-      @invite.token.should be_kind_of(String)
-      @invite.token.length.should >= 40
-    end
   end
-  
-  describe '#destroy' do
-    it 'grants the inviter a new invite' do
-      invite = create(:invite)
-      expect { invite.destroy }.to change{invite.user.available_invites}.by(1)
+
+  describe "before_create" do
+
+    subject { invite }
+    its(:expires_at) { should be_within(30).of(Time.now + 14.days) }
+    its(:token) { should be_kind_of(String) }
+    its("token.length") { should >= 40 }
+
+    it "revokes an invite from the inviter" do
+      inviter = create(:user, :available_invites => 1)
+      expect {
+        create(:invite, :user => inviter)
+      }.to change{inviter.available_invites}.by(-1)
     end
+
   end
-  
-  describe '#find_active' do
-    before do
-      5.times { create(:invite) }
-      5.times { create(:invite, :expires_at => 2.days.ago) }
-      @invites = Invite.find_active
+
+  describe "before_destroy" do
+
+    context "when invite has been used" do
+      before { invite.used = true }
+      it "doesn't grant the user an invite" do
+        expect {
+          invite.destroy
+        }.not_to change{ invite.user.available_invites }
+      end
     end
 
-    it 'finds all active invites' do
-      @invites.length.should == 5
-    end
-
-    it 'does not find expired invites' do
-      @invites.select(&:expired?).length == 0
+    context "when invite hasn't been used" do
+      it "grants the inviter back an invite" do
+        expect {
+          invite.destroy
+        }.to change{ invite.user.available_invites }.by(1)
+      end
     end
   end
 
-  describe '#find_expired' do
-    before do
-      5.times { create(:invite) }
-      5.times { create(:invite, :expires_at => 2.days.ago) }
-      @invites = Invite.find_expired
-    end
-
-    it 'finds all expired invites' do
-      @invites.length.should == 5
-    end
-
-    it 'finds only expired invites' do
-      @invites.select(&:expired?).length == 5
-    end
+  describe ".unique_token" do
+    subject { Invite.unique_token }
+    it { should be_kind_of(String) }
+    its(:length) { should >= 40 }
   end
 
-  context 'expired' do
+  describe ".expiration_time" do
+    subject { Invite.expiration_time }
+    it { should == 14.days }
+  end
+
+  describe ".find_active" do
+
     before do
-      @invite = create(:invite, :expires_at => 2.days.ago)
+      invite
+      expired_invite
     end
 
-    it 'is expired' do
-      @invite.expired?.should be_true
+    subject { Invite.find_active }
+    it { should == [invite] }
+
+  end
+
+  describe ".find_expired" do
+
+    before do
+      invite
+      expired_invite
     end
 
-    it 'is cleaned up by destroy_expired!' do
+    subject { Invite.find_expired }
+    it { should == [expired_invite] }
+
+  end
+
+  describe ".destroy_expired!" do
+
+    before do
+      invite
+      expired_invite
+    end
+
+    it "destroys all expired invites" do
+      expect {
+        Invite.destroy_expired!
+      }.should change{ Invite.count }.by(-1)
+    end
+
+  end
+
+  describe "#expired?" do
+    specify { invite.expired?.should be_false }
+    specify { expired_invite.expired?.should be_true }
+  end
+
+  describe "#expire!" do
+    before { expired_invite }
+    it "destroys the invite" do
       expect { Invite.destroy_expired! }.to change{Invite.count}.by(-1)
     end
   end
-  
+
 end
