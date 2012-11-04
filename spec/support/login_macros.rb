de8ac1f86129b272bd9ebd54ef2054c452936f9c
Inge Jørgensen <inge@manualdesign.no>
2012-11-04 21:26:00 +0100

    LoginMacros for access control testing

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -4,14 +4,90 @@ module LoginMacros
   end
 
   module ClassMethods
+
     def it_requires_login_for(*actions)
       actions.each do |action|
-        it "#{action} action should require login" do
+        it "requires login for #{action} action" do
+          logout
           get action, :id => 1
           response.should redirect_to(login_users_url)
         end
       end
     end
+
+    def it_requires_authentication_for(*actions)
+      actions.each do |action|
+        it "requires authentication for #{action}" do
+          previous_config = Sugar.config(:public_browsing)
+          Sugar.config(:public_browsing, false)
+          controller.should_receive(:require_user_account)
+            .at_least(:once)
+            .and_return(true)
+          get action, :id => 1
+          Sugar.config(:public_browsing, previous_config)
+        end
+      end
+    end
+
+    def it_requires_user_for(*actions)
+      actions.each do |action|
+        it "requires a user for #{action}" do
+          controller.should_receive(:require_user_account)
+            .at_least(:once)
+            .and_return(true)
+          get action, :id => 1
+        end
+      end
+    end
+
+    def it_requires_admin_for(*actions)
+      actions.each do |action|
+        it "requires an admin for #{action}" do
+          received_option = false
+          controller.should_receive(:verify_user) { |options|
+            if options[:admin]
+              received_option = true
+            end
+            true
+          }.at_least(:once)
+          get action, :id => 1
+          received_option.should be_true
+        end
+      end
+    end
+
+    def it_requires_moderator_for(*actions)
+      actions.each do |action|
+        it "requires a moderator for #{action}" do
+          received_option = false
+          controller.should_receive(:verify_user) { |options|
+            if options[:moderator]
+              received_option = true
+            end
+            true
+          }.at_least(:once)
+          get action, :id => 1
+          received_option.should be_true
+        end
+      end
+    end
+
+    def it_requires_user_admin_for(*actions)
+      actions.each do |action|
+        it "requires a user admin for #{action}" do
+          received_option = false
+          controller.should_receive(:verify_user) { |options|
+            if options[:user_admin]
+              received_option = true
+            end
+            true
+          }.at_least(:once)
+          get action, :id => 1
+          received_option.should be_true
+        end
+      end
+    end
+
   end
 
   def login(user=nil)
@@ -20,4 +96,11 @@ module LoginMacros
     session[:hashed_password] = @current_user.hashed_password
     session[:ips] = ['0.0.0.0']
   end
+
+  def logout
+    session[:user_id] = nil
+    session[:hashed_password] = nil
+    session[:ips] = []
+  end
+
 end
