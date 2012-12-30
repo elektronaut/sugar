deeb341efd55b686cb1dca002bff91bb88a1291f
Inge Jørgensen <inge@manualdesign.no>
2012-12-30 22:06:00 +0100

    InvitesController specs

--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -3,155 +3,177 @@
 require 'spec_helper'
 
 describe InvitesController do
+
+  let(:invite)            { create(:invite) }
+  let(:expired_invite)    { create(:invite, expires_at: 2.days.ago) }
+  let(:user)              { create(:user) }
+  let(:user_with_invites) { create(:user, available_invites: 1) }
+
   it_requires_login_for :index, :all, :new, :create, :destroy
+  it_requires_user_admin_for :all
 
-  describe 'with no available invites' do
-    before { login create(:user, :available_invites => 0) }
 
-    [:new, :create].each do |action|
-      describe "GET #{action}" do
-        before { get action }
-        it { should set_the_flash.to(/You don't have any invites!/) }
-        it { should respond_with(:redirect) }
-      end
-    end
-  end
-
-  describe 'with available invites' do
+  describe "#load_invite" do
     before do
-      login(@user = create(:user, :available_invites => 5))
+      login invite.user
+      delete :destroy, id: invite_id
     end
 
-    describe 'GET new' do
-      before { get :new }
-      it { should_not set_the_flash }
-      it { should render_template(:new) }
+    context "when invite exists" do
+      let(:invite_id) { invite.id }
       it { should assign_to(:invite).with_kind_of(Invite) }
     end
 
-    describe 'POST create' do
-      describe 'with a valid invitee' do
-        before do
-          post :create, :invite => {
-            :email   => 'no-reply@example.com',
-            :message => 'testing message'
-          }
-        end
-        it { should assign_to(:invite).with_kind_of(Invite) }
-        it { should set_the_flash.to(/Your invite has been sent/) }
-        it { should respond_with(:redirect) }
-        it 'redirects back to invites' do
-          response.should redirect_to(invites_url)
-        end
-        it 'decrements available invites' do
-          @user.reload
-          @user.available_invites.should == 4
-        end
-        it 'sends a welcome email' do
-          email = ActionMailer::Base.deliveries.first
-          email.to[0].should eq('no-reply@example.com')
-          email.body.to_s.should match(/testing message/)
-        end
-      end
-
-      describe 'with an already existing user' do
-        before do
-          post :create, :invite => {
-            :email => create(:user).email
-          }
-          it { should_not set_the_flash }
-          it { should render_template(:new) }
-          it { should assign_to(:invite).with_kind_of(Invite) }
-          it 'does not decrement available invites' do
-            @user.reload
-            @user.available_invites.should == 5
-          end
-        end
-      end
+    context "when invite doesn't exist" do
+      let(:invite_id) { 1231115 }
+      it { should_not assign_to(:invite) }
+      it { should respond_with(404) }
     end
   end
 
-  describe 'GET all' do
-    describe 'as a regular user' do
-      before do
-        login
-        get :all
-      end
+  describe "#verify_invites" do
+    before { login(user); get :new }
+
+    context "when user has invites" do
+      let(:user) { create(:user, available_invites: 1) }
+      it { should respond_with(:success) }
+      it { should_not set_the_flash.to(/You don't have any invites!/) }
+    end
+
+    context "when user is user admin" do
+      let(:user) { create(:user_admin) }
+      it { should respond_with(:success) }
+      it { should_not set_the_flash.to(/You don't have any invites!/) }
+    end
+
+    context "when user doesn't have any invites" do
+      let(:user) { create(:user, available_invites: 0) }
       it { should set_the_flash }
       it { should respond_with(:redirect) }
-      it 'redirects back to discussions' do
-        response.should redirect_to(discussions_url)
+    end
+  end
+
+  describe "GET index" do
+    let!(:invites) { [create(:invite, user: user), create(:invite)] }
+    before { login(user); get :index }
+
+    it { should respond_with(:success) }
+    it { should render_template(:index) }
+    it { should_not set_the_flash }
+    it { should assign_to(:invites).with_kind_of(Enumerable) }
+    specify { assigns(:invites).should =~ [invites.first] }
+  end
+
+  describe "GET all" do
+    let(:user) { create(:user_admin) }
+    let!(:invites) { [create(:invite, user: user), create(:invite)] }
+
+    before { login(user); get :all }
+
+    it { should respond_with(:success) }
+    it { should render_template(:all) }
+    it { should_not set_the_flash }
+    it { should assign_to(:invites).with_kind_of(Enumerable) }
+    specify { assigns(:invites).should =~ invites }
+  end
+
+  describe "GET accept" do
+    before { get :accept, id: token }
+
+    context "when invite is valid" do
+      let(:token) { invite.token }
+      it { should_not set_the_flash }
+      specify { session[:invite_token].should == invite.token }
+      it "redirects to the signup page" do
+        response.should redirect_to(new_user_by_token_url(:token => invite.token))
+      end
+    end
+
+    context "when invite is expired" do
+      let(:token) { expired_invite.token }
+      it { should set_the_flash.to(/Your invite has expired!/) }
+      specify { session[:invite_token].should be_nil }
+      it "redirects to the login page" do
+        response.should redirect_to(login_users_url)
+      end
+    end
+
+    context "when invite doesn't exist" do
+      let(:token) { "invalid token" }
+      it { should set_the_flash.to(/That's not a valid invite!/) }
+      specify { session[:invite_token].should be_nil }
+      it "redirects to the login page" do
+        response.should redirect_to(login_users_url)
       end
     end
   end
 
-  describe 'when logged in as a user admin' do
-    before do
-      login(@user = create(:user_admin, :available_invites => 0))
-    end
-    describe 'GET all' do
-      before { get :all }
-      it { should_not set_the_flash }
-      it { should respond_with(:success) }
-      it { should render_template(:all) }
-      it { should assign_to(:invites) }
-    end
+  describe "GET new" do
+    before { login(user_with_invites); get :new }
+    it { should respond_with(:success) }
+    it { should render_template(:new) }
+    it { should_not set_the_flash }
+    it { should assign_to(:invite).with_kind_of(Invite) }
+  end
 
-    describe 'POST create' do
+  describe "POST create" do
+    before { login(user_with_invites) }
+
+    context "with valid params" do
       before do
-        post :create, :invite => {
-          :email   => 'no-reply@example.com',
-          :message => 'testing message'
+        post :create, invite: {
+          email:   'no-reply@example.com',
+          message: 'testing message'
         }
       end
+
       it { should assign_to(:invite).with_kind_of(Invite) }
-      it { should set_the_flash.to(/Your invite has been sent/) }
-      it { should respond_with(:redirect) }
+      it { should set_the_flash.to(/Your invite has been sent to no\-reply@example\.com/) }
+
+      it "sends a welcome email" do
+        email = ActionMailer::Base.deliveries.first
+        email.to[0].should eq('no-reply@example.com')
+        email.body.to_s.should match(/testing message/)
+      end
+
+      it "redirects to the invites page" do
+        response.should redirect_to(invites_url)
+      end
+    end
+
+    context "when email is invalid" do
+      pending
+    end
+
+    context "with invalid params" do
+      before { post :create, :invite => {} }
+      it { should assign_to(:invite).with_kind_of(Invite) }
+      it { should_not set_the_flash }
+      it { should respond_with(:success) }
+      it { should render_template(:new) }
     end
   end
 
+  describe "DELETE destroy" do
+    context "when user owns the invite" do
+      before { login(invite.user) and delete(:destroy, id: invite.id) }
 
-  describe 'GET accept' do
-    describe 'with an existing invite' do
-      before { @invite = create(:invite) }
-      before { get :accept, :id => @invite.token }
       it { should assign_to(:invite).with_kind_of(Invite) }
-      it { should_not set_the_flash }
-      it 'sets the session token' do
-        session[:invite_token].should eq(@invite.token)
-      end
-      it 'redirects to the new user page' do
-        response.should redirect_to(new_user_by_token_url(:token => @invite.token))
+      specify { assigns(:invite).destroyed?.should be_true }
+
+      it "redirects to the invites page" do
+        response.should redirect_to(invites_url)
       end
     end
 
-    describe 'with an expired invite' do
-      before { @invite = create(:invite, :expires_at => 90.days.ago) }
-      before { get :accept, :id => @invite.token }
-      it { should assign_to(:invite).with_kind_of(Invite) }
-      it { should set_the_flash.to(/Your invite has expired/) }
-      it 'deletes the invite' do
-        Invite.exists?(@invite).should be_false
-      end
-      it 'does not set the session token' do
-        session[:invite_token].should be_nil
-      end
-      it 'redirects to the login page' do
-        response.should redirect_to(login_users_url)
-      end
-    end
+    context "when user doesn't own the invite" do
+      before { login(user) and delete(:destroy, id: invite.id) }
 
-    describe 'with an invalid token' do
-      before { get :accept, :id => 'invalidtoken' }
-      it 'does not find an invite' do
-        assigns(:invite).should be_nil
-      end
-      it { should set_the_flash.to(/not a valid invite/) }
-      it 'does not set the session token' do
-        session[:invite_token].should be_nil
-      end
-      it 'redirects to the login page' do
-        response.should redirect_to(login_users_url)
+      it { should assign_to(:invite).with_kind_of(Invite) }
+      specify { assigns(:invite).destroyed?.should be_false }
+
+      it "redirects to the discussions page" do
+        response.should redirect_to(discussions_url)
       end
     end
   end
