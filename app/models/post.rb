d36132b6bef8732392cca4f90271252b4009066f
Inge Jørgensen <inge@manualdesign.no>
2013-01-29 21:46:04 +0100

    Don't mark posts as edited if they've been edited within 60 seconds of creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -110,7 +110,7 @@ class Post < ActiveRecord::Base
 
   def edited?
     return false unless edited_at?
-    (((self.edited_at || self.created_at) - self.created_at) > 5.seconds ) ? true : false
+    (((self.edited_at || self.created_at) - self.created_at) > 60.seconds ) ? true : false
   end
 
   def editable_by?(user)
