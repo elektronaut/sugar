c482ebbc92a1deb6cee5308419bc15ede03dc455
Inge Jørgensen <inge@manualdesign.no>
2013-02-08 13:59:09 +0100

    Reduced complexity in OpenidController

--- app/controllers/openid_controller.rb
+++ app/controllers/openid_controller.rb
@@ -5,42 +5,31 @@ class OpenidController < ApplicationController
   # Start an authentication session
   def create
     unless start_openid_session(
-      params[:openid_url], 
-      :immediate => false, 
-      :success   => params[:openid_success], 
+      params[:openid_url],
+      :immediate => false,
+      :success   => params[:openid_success],
       :fail      => params[:openid_success]
     )
       fail_authentication('Not a valid OpenID URL') and return
     end
   end
-  
+
   # Complete the OpenID authentication
   def complete
-    response_params = params.dup
-    response_params.delete(:controller)
-    response_params.delete(:action)
-    response_params.delete(:format)
-    response = openid_consumer.complete(response_params, complete_openid_url)
-
-    case response
+    case response = openid_complete_response
 
     # Setup needed
     when ::OpenID::Consumer::SetupNeededResponse
-      setup_url = response.instance_eval{ @setup_url } rescue nil
-      if setup_url
+      if setup_url = openid_setup_url(response)
         redirect_to setup_url and return
-      else
-        setup_response = openid_consumer.begin(response.identity_url) rescue nil
-        if setup_response
-          perform_openid_authentication(response) and return
-        end
+      elsif setup_response(response)
+        perform_openid_authentication(response) and return
       end
 
     # Authentication success
     when ::OpenID::Consumer::SuccessResponse
       session[:authenticated_openid_url] = ::OpenID.normalize_url(response.identity_url)
-      redirect_url = session[:openid_redirect_success] || root_url
-      redirect_to redirect_url and return
+      redirect_to openid_redirect_url and return
 
     # Authentication failed
     when ::OpenID::Consumer::FailureResponse
@@ -49,14 +38,46 @@ class OpenidController < ApplicationController
 
     fail_authentication and return
   end
-  
+
   protected
-  
+
+    def openid_redirect_url
+      if session[:openid_redirect_success]
+        session[:openid_redirect_success]
+      else
+        root_url
+      end
+    end
+
+    def setup_response(response)
+      begin
+        openid_consumer.begin(response.identity_url)
+      rescue
+        nil
+      end
+    end
+
+    def openid_setup_url(response)
+      begin
+        response.instance_eval{ @setup_url }
+      rescue
+        nil
+      end
+    end
+
+    def openid_complete_response
+      response_params = params.dup
+      response_params.delete(:controller)
+      response_params.delete(:action)
+      response_params.delete(:format)
+      openid_consumer.complete(response_params, complete_openid_url)
+    end
+
     def fail_authentication(message='OpenID login failed')
       session[:openid_url] = nil
       flash[:notice] = message
       redirect_url = session[:openid_redirect_fail] || root_url
       redirect_to redirect_url
     end
-  
+
 end
