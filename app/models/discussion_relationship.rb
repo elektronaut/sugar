e030adf0abd464412f089118793c45cb6b649048
Inge Jørgensen <inge@manualdesign.no>
2013-02-14 01:29:19 +0100

    ..which means the nasty finder methods in DiscussionRelationship are toast

--- app/models/discussion_relationship.rb
+++ app/models/discussion_relationship.rb
@@ -21,65 +21,6 @@ class DiscussionRelationship < ActiveRecord::Base
       relationship.save
       relationship
     end
-
-    # Find participated discussions for a user
-    def find_participated(user, options={})
-      self.find_discussions(user, {:participated => true}.merge(options))
-    end
-
-    # Find followed discussions for a user
-    def find_following(user, options={})
-      self.find_discussions(user, {:following => true}.merge(options))
-    end
-
-    # Find favorite discussions for a user
-    def find_favorite(user, options={})
-      self.find_discussions(user, {:favorite => true}.merge(options))
-    end
-
-    def find_discussions(user, options={})
-      conditions = options.select do |k, v|
-        [:participated, :following, :favorite].include?(k)
-      end
-      conditions[:user_id] = user.id
-      conditions[:trusted] = false unless options[:trusted]
-
-      discussions = Discussion
-        .select("#{Discussion.table_name}.*")
-        .order('sticky DESC, last_post_at DESC')
-        .includes(:poster, :last_poster, :category)
-        .joins(:discussion_relationships)
-        .where(:discussion_relationships => conditions)
-
-      if options.has_key?(:page)
-        limit     = options[:limit] || Exchange.per_page
-        discussions_count = self.count(:all, :conditions => conditions)
-        num_pages = (discussions_count.to_f/limit).ceil
-        page      = (options[:page] || 1).to_i
-        page      = 1 if page < 1
-        page      = num_pages if page > num_pages
-        offset    = limit * (page - 1)
-        offset    = 0 if offset < 0
-
-        discussions = discussions
-          .limit(limit)
-          .offset(offset)
-      end
-
-      discussions = discussions.all
-
-      if options.has_key?(:page)
-        Pagination.apply(
-          discussions,
-          Pagination::Paginater.new(
-            :total_count => discussions_count,
-            :page => page,
-            :per_page => limit
-          )
-        )
-      end
-      discussions
-    end
   end
 
   def update_user_caches!
