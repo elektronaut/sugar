8c56c7d8f9bebaf77c0ef562aa812e62e10280b2
Inge Jørgensen <inge@manualdesign.no>
2013-05-02 00:40:10 +0200

    Always use test environment for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ Spork.prefork do
   end
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
-  ENV["RAILS_ENV"] ||= 'test'
+  ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
