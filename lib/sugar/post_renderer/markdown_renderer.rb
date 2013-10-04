5dfd803b3b0b9bd920687cae60009c58a266c570
Inge Jørgensen <inge@manualdesign.no>
2013-10-04 14:37:54 +0200

    Markdown syntax for blockquotes now require a space

--- lib/sugar/post_renderer/markdown_renderer.rb
+++ lib/sugar/post_renderer/markdown_renderer.rb
@@ -10,6 +10,14 @@ module Sugar
           "<a href=\"#{link}\">#{link}</a>"
         end
       end
+
+      def preprocess(document)
+        escape_leading_gts_without_space(document)
+      end
+
+      def escape_leading_gts_without_space(document)
+        document.gsub(/^([\s]*)([>]+)([^\s>])/) { $1 + ("&gt;" * $2.length) + $3 }
+      end
     end
   end
 end
