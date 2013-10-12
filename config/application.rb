2f2c8d977aecfb05216f14dcbf0580ea8d71ea7e
Inge Jørgensen <inge@manualdesign.no>
2013-10-12 17:22:14 +0200

    Moved Authentication to concerns

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Sugar
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(
       #{config.root}/lib
-      #{config.root}/app/models/concerns
     )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
