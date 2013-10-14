ec8d075fc665aafb02210c0517ec3f7901e5abb4
Inge Jørgensen <inge@manualdesign.no>
2013-10-15 00:25:38 +0200

    Fix for conversation post count cleaner

--- app/models/post_observer.rb
+++ app/models/post_observer.rb
@@ -3,20 +3,21 @@
 class PostObserver < ActiveRecord::Observer
   observe Post
 
-  def clean_cache_for(record)
-    # Clean posts count cache
-    cache_file = Rails.root.join("public/cache/discussions/#{record.exchange_id}/posts/count.json")
+  # Clean posts count cache
+  def clean_cache_for(post)
+    exchange_type = post.exchange.type.downcase
+    cache_file = Rails.root.join("public/cache/#{exchange_type}s/#{post.exchange_id}/posts/count.json")
     if File.exists?(cache_file)
       File.unlink(cache_file)
     end
   end
 
-  def after_create(record)
-    clean_cache_for(record)
+  def after_create(post)
+    clean_cache_for(post)
   end
 
-  def after_destroy(record)
-    clean_cache_for(record)
+  def after_destroy(post)
+    clean_cache_for(post)
   end
 
 end
