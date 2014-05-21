9f2038dc14cde57551b7fab3d0e5b3a6b6b543e5
Inge Jørgensen <inge@manualdesign.no>
2014-05-22 00:51:14 +0200

    Add mobile assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Sugar
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+    config.assets.precompile += %w( mobile.js )
   end
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,6 @@ Sugar::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mobile.js )
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
