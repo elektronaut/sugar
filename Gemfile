2c421daca7f3f53e3c0f0b30003b4d8a8ad03511
Inge Jørgensen <inge@manualdesign.no>
2014-06-12 15:46:06 +0200

    Report coverage to CodeClimate

--- Gemfile
+++ Gemfile
@@ -74,6 +74,7 @@ group :development_mac do
 end
 
 group :test do
+  gem 'codeclimate-test-reporter', require: false
 end
 
 group :test, :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 ENV["RAILS_ENV"] = 'test'
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'rubygems'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
