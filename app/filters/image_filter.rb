628603b850127dab767a8ff2b398faa6948359d1
Inge Jørgensen <inge@manualdesign.no>
2015-03-06 19:44:39 +0100

    Improves ImageFilter

--- app/filters/image_filter.rb
+++ app/filters/image_filter.rb
@@ -4,18 +4,36 @@ class ImageFilter < Filter
   def process(post)
     parser = Nokogiri::HTML::DocumentFragment.parse(post)
     parser.css("img").each do |element|
-      if element.attributes && !element.attributes["src"].blank?
-        url = element.attributes["src"].to_s
-        if element.attributes["width"].blank? ||
-            element.attributes["height"].blank?
-          if dimensions = FastImage.size(url, timeout: 2.0)
-            width, height = dimensions
-            element.set_attribute "width", width.to_s
-            element.set_attribute "height", height.to_s
-          end
-        end
-      end
+      process_image(element)
     end
     parser.to_html
+  end
+
+  private
+
+  def has_src?(elem)
+    elem.attributes && !elem.attributes["src"].blank?
+  end
+
+  def image_src(elem)
+    if has_src?(elem)
+      elem.attributes["src"].to_s
+    end
+  end
+
+  def needs_dimensions?(elem)
+    elem.attributes["width"].blank? ||
+      elem.attributes["height"].blank?
+  end
+
+  def process_image(elem)
+    url = image_src(elem)
+    if url && needs_dimensions?(elem)
+      if dimensions = FastImage.size(url, timeout: 2.0)
+        width, height = dimensions
+        elem.set_attribute "width", width.to_s
+        elem.set_attribute "height", height.to_s
+      end
+    end
   end
 end
