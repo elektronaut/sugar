88f4210867afd2da5a0f76cd1f106646be7b5115
Inge Jørgensen <inge@manualdesign.no>
2016-01-18 18:09:26 +0100

    Fixes specs on Postgres

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,8 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+  config.use_transactional_examples = false
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -43,7 +45,7 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
