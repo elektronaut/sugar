5f831141694100a108fd5693cbbfb879217e9902
Daniel Vena <deaconvena+git@gmail.com>
2017-06-08 13:12:57 -0400

    Scope twitter widget update to #ajaxPosts

--- app/assets/javascripts/sugar/posts/new_posts.es6.jsx
+++ app/assets/javascripts/sugar/posts/new_posts.es6.jsx
@@ -103,7 +103,7 @@ Sugar.loadNewPosts = function() {
 
       // Update Twitter embeds
       if (twttr && twttr.widgets) {
-        twttr.widgets.load();
+        twttr.widgets.load(document.getElementById('#ajaxPosts'));
       }
 
       // Animate in the new posts
