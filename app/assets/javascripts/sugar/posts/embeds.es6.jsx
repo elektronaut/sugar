1e7a090e46e1ee633be07846d77eb5882da28507
Daniel Vena <deaconvena+git@gmail.com>
2017-06-10 21:33:37 -0400

    physically scroll page instead of using anchor trick
    anchor trick was causing odd page jumps and breaking the back button

--- app/assets/javascripts/sugar/posts/embeds.es6.jsx
+++ app/assets/javascripts/sugar/posts/embeds.es6.jsx
@@ -5,12 +5,18 @@ $(Sugar).bind('ready', function(){
   
   // Re-establish anchor position on initial page's twitter widgets load
   if ($('.twitter-tweet,twitterwidget').length && twttr && twttr.events) {
-    twttr.events.bind('loaded', function refocus() {
-      var a = window.location.hash;
-      window.location.hash = '#';
-      window.location.hash = a;
-      twttr.events.unbind('loaded', refocus);
-    });
+    var anchor = $('a[name="' + window.location.hash.substr(1) + '"]');
+    if (anchor.length) {
+      function scrollPage() {
+        window.scrollTo(0, anchor.offset().top);
+      }
+      twttr.events.bind('rendered', scrollPage);
+      twttr.events.bind('loaded', function tweetsLoaded() {
+        scrollPage();
+        twttr.events.unbind('rendered', scrollPage);
+        twttr.events.unbind('loaded', tweetsLoaded);
+      });
+    }
   }
 
   // Initialize twitter embeds when new posts load or previewed
