2801dc5736fe6d833d09b9a9b0cac446f916f1ba
Inge Jørgensen <inge@anyone.no>
2020-10-01 15:47:29 +0200

    Update Rails defaults to 6.0

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,10 @@ Bundler.require(*Rails.groups)
 module Sugar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W[#{config.root}/lib]
+    config.eager_load_paths += %W[#{config.root}/lib]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
