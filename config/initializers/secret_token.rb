86e73b5ffa38aacba19bcf285cd1dc79319db1dd
Inge Jørgensen <inge@elektronaut.no>
2022-02-06 04:40:12 +0100

    Ignore local .env files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-unless ENV["SUGAR_SECRET_KEY_BASE"]
-  raise "SUGAR_SECRET_KEY_BASE environment variable hasn't been set."
-end
+Dotenv.require_keys("SUGAR_SECRET_KEY_BASE")
 
 Sugar::Application.config.secret_key_base = ENV["SUGAR_SECRET_KEY_BASE"]
