0896e824cb5080d4dca6ca4679d8e0ab2888d391
Inge Jørgensen <inge@elektronaut.no>
2022-02-07 15:14:21 +0100

    Fix SUGAR_SECRET_KEY_BASE check, Dotenv is not available in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Dotenv.require_keys("SUGAR_SECRET_KEY_BASE")
+unless ENV["SUGAR_SECRET_KEY_BASE"]
+  raise "SUGAR_SECRET_KEY_BASE environment variable hasn't been set."
+end
 
 Sugar::Application.config.secret_key_base = ENV["SUGAR_SECRET_KEY_BASE"]
