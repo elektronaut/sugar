1b4be05beb4d717c13796e404e019ee6d4a543ce
Inge Jørgensen <inge@elektronaut.no>
2022-11-01 22:21:25 +0100

    Check that posts exist before applying the resizeObserver

--- app/javascript/sugar/posts/embeds.js
+++ app/javascript/sugar/posts/embeds.js
@@ -5,6 +5,7 @@ import Sugar from "../../sugar";
  * Handlers for scripted embed (social) quirks
  */
 $(Sugar).bind("ready", function(){
+  const postsContainer = document.querySelector(".posts");
 
   // Initialize twitter embeds when new posts load or previewed
   $(Sugar).bind("postsloaded", function(event, posts) {
@@ -13,21 +14,22 @@ $(Sugar).bind("ready", function(){
     }
   });
 
-  var scrollHeightCache = document.documentElement.scrollHeight;
-  var scrollYCache = window.scrollY;
-  const resizeObserver = new ResizeObserver(function (entries) {
-    entries.forEach(function () {
-      if (scrollYCache !== window.scrollY) {
-        // Chrome updates the scroll position, but not the scroll!
-        window.scrollTo(window.scrollX, window.scrollY);
-      } else if (document.documentElement.scrollHeight !== scrollHeightCache) {
-        var scrollYBy = document.documentElement.scrollHeight - scrollHeightCache;
-        window.scrollBy(0, scrollYBy);
-      }
-      scrollYCache = window.scrollY;
-      scrollHeightCache = document.documentElement.scrollHeight;
+  if (postsContainer) {
+    let scrollHeightCache = document.documentElement.scrollHeight;
+    let scrollYCache = window.scrollY;
+    const resizeObserver = new ResizeObserver(function (entries) {
+      entries.forEach(function () {
+        if (scrollYCache !== window.scrollY) {
+          // Chrome updates the scroll position, but not the scroll!
+          window.scrollTo(window.scrollX, window.scrollY);
+        } else if (document.documentElement.scrollHeight !== scrollHeightCache) {
+          const scrollYBy = document.documentElement.scrollHeight - scrollHeightCache;
+          window.scrollBy(0, scrollYBy);
+        }
+        scrollYCache = window.scrollY;
+        scrollHeightCache = document.documentElement.scrollHeight;
+      });
     });
-  });
-
-  resizeObserver.observe(document.querySelector(".posts"));
+    resizeObserver.observe(postsContainer);
+  }
 });
