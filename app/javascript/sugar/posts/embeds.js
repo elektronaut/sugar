63725e2349b555168d40f6458264377292aa5d70
Daniel Vena <deaconvena+github@gmail.com>
2022-11-01 11:30:20 -0400

    implement twitter embed render watch for scroll anchoring

--- app/javascript/sugar/posts/embeds.js
+++ app/javascript/sugar/posts/embeds.js
@@ -5,10 +5,34 @@ import Sugar from "../../sugar";
  * Handlers for scripted embed (social) quirks
  */
 $(Sugar).bind("ready", function(){
+  // Twitter doesn't have unbind, so we'll store our escape hatch
+  var disableTwitterAnchorScroll = false;
+
   // Initialize twitter embeds when new posts load or previewed
   $(Sugar).bind("postsloaded", function(event, posts) {
     if (posts.length && window.twttr && window.twttr.widgets) {
+      // We don't want to scroll to an anchor at this point
+      disableTwitterAnchorScroll = true;
       window.twttr.widgets.load(posts[0].parentNode);
     }
   });
+
+  // Handle updating scroll position to anchor for twitter embed loads
+  function updateAnchorScroll() {
+    if (disableTwitterAnchorScroll) {
+      return;
+    }
+    var postId = window.location.hash.match(/(post\-\d+)/);
+    if (postId.length) {
+      var rPost = document.querySelector('a[name="' + postId[1] + '"].anchor');
+      if (rPost) {
+        rPost.scrollIntoView();
+      }
+    }
+  }
+  if (window.twttr) {
+    twttr.ready(function (twttr) {
+      twttr.events.bind('rendered', updateAnchorScroll);
+    });
+  }
 });
