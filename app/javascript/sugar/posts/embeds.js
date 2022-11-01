96ddc7d1cfaa8944d7fbe9e9ac29ec24855ebb1b
Daniel Vena <deaconvena+github@gmail.com>
2022-11-01 16:29:11 -0400

    universal scroll anchoring with resizeObserver

--- app/javascript/sugar/posts/embeds.js
+++ app/javascript/sugar/posts/embeds.js
@@ -5,34 +5,29 @@ import Sugar from "../../sugar";
  * Handlers for scripted embed (social) quirks
  */
 $(Sugar).bind("ready", function(){
-  // Twitter doesn't have unbind, so we'll store our escape hatch
-  var disableTwitterAnchorScroll = false;
 
   // Initialize twitter embeds when new posts load or previewed
   $(Sugar).bind("postsloaded", function(event, posts) {
     if (posts.length && window.twttr && window.twttr.widgets) {
-      // We don't want to scroll to an anchor at this point
-      disableTwitterAnchorScroll = true;
       window.twttr.widgets.load(posts[0].parentNode);
     }
   });
 
-  // Handle updating scroll position to anchor for twitter embed loads
-  function updateAnchorScroll() {
-    if (disableTwitterAnchorScroll) {
-      return;
-    }
-    var postId = window.location.hash.match(/(post-\d+)/);
-    if (postId.length) {
-      var rPost = document.querySelector("a[name='" + postId[1] + "'].anchor");
-      if (rPost) {
-        rPost.scrollIntoView();
+  var scrollHeightCache = document.documentElement.scrollHeight;
+  var scrollYCache = window.scrollY;
+  const resizeObserver = new ResizeObserver(function (entries) {
+    entries.forEach(function () {
+      if (scrollYCache !== window.scrollY) {
+        // Chrome updates the scroll position, but not the scroll!
+        window.scrollTo(window.scrollX, window.scrollY);
+      } else if (document.documentElement.scrollHeight !== scrollHeightCache) {
+        var scrollYBy = document.documentElement.scrollHeight - scrollHeightCache;
+        window.scrollBy(0, scrollYBy);
       }
-    }
-  }
-  if (window.twttr) {
-    window.twttr.ready(function (twttr) {
-      twttr.events.bind("rendered", updateAnchorScroll);
+      scrollYCache = window.scrollY;
+      scrollHeightCache = document.documentElement.scrollHeight;
     });
-  }
+  });
+
+  resizeObserver.observe(document.querySelector(".posts"));
 });
