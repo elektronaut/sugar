a78e9cf49e4b17a84c666bcb5de8f9343c90828d
Inge Jørgensen <inge@elektronaut.no>
2022-12-24 04:20:13 +0100

    Update dependencies

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,6 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Emoji.images_path
-
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
