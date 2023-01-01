8ab8050a9160c2a29b120fa8eeb3fdeae46cd695
Inge Jørgensen <inge@elektronaut.no>
2023-01-01 12:34:48 +0100

    User links: Drag and drop sorting

--- app/javascript/components/EditUserLinks/List.jsx
+++ app/javascript/components/EditUserLinks/List.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  DndContext,
+  closestCenter,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 import UserLink from "./UserLink";
 
 export default function List(props) {
   const { dispatch, userLinks } = props;
 
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  const handleDragEnd = (evt) => {
+    const { active, over } = evt;
+
+    if (active.id !== over.id) {
+      const ids = userLinks.map(o => o.handle);
+      const oldIndex = ids.indexOf(active.id);
+      const newIndex = ids.indexOf(over.id);
+      props.dispatch({
+        type: "reorder",
+        payload: arrayMove(userLinks, oldIndex, newIndex)
+      });
+    }
+  };
+
   return (
     <div className="list">
-      {userLinks.map((ul, index) =>
-        <UserLink key={ul.handle}
-                  dispatch={dispatch}
-                  userLink={ul}
-                  position={index + 1} />)}
+      <DndContext sensors={sensors}
+                  collisionDetection={closestCenter}
+                  onDragEnd={handleDragEnd}>
+        <SortableContext items={userLinks.map(ul => ul.handle)}
+                         strategy={verticalListSortingStrategy}>
+          {userLinks.map((ul, index) =>
+            <UserLink key={ul.handle}
+                      dispatch={dispatch}
+                      userLink={ul}
+                      position={index + 1} />)}
+        </SortableContext>
+      </DndContext>
     </div>
   );
 }
--- app/javascript/components/EditUserLinks/UserLink.jsx
+++ app/javascript/components/EditUserLinks/UserLink.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function label(userLink) {
   return userLink.name || userLink.url.replace(/^(f|ht)tps?:\/\//, "");
 }
 
 export default function UserLink(props) {
-  const { dispatch } = props;
-  const userLink = props.userLink;
+  const { dispatch, userLink } = props;
+
+  const { attributes,
+          isDragging,
+          listeners,
+          setNodeRef,
+          transform,
+          transition } = useSortable({ id: userLink.handle });
+
+  const style = { transform: CSS.Transform.toString(transform),
+                  transition };
 
   const handleEdit = (evt) => {
     evt.preventDefault();
@@ -19,8 +30,19 @@ export default function UserLink(props) {
     dispatch({ type: "delete", payload: userLink });
   };
 
+  let classNames = ["user-link"];
+  if (isDragging) {
+    classNames.push("dragging");
+  }
+
   return (
-    <div className="user-link">
+    <div className={classNames.join(" ")}
+         ref={setNodeRef}
+         style={style}
+         {...attributes}>
+      <div className="drag-handle" {...listeners}>
+        <i className="fa fa-arrows"></i>
+      </div>
       <div className="info">
         <div className="label">
           {userLink.label}
@@ -31,14 +53,15 @@ export default function UserLink(props) {
            <a href={userLink.url}>{label(userLink)}</a>}
         </div>
       </div>
-      <div className="buttons">
-        <button type="button" onClick={handleEdit}>
-          Edit
-        </button>
-        <button type="button" onClick={handleDelete}>
-          Remove
-        </button>
-      </div>
+      {!isDragging &&
+       <div className="buttons">
+         <button type="button" onClick={handleEdit}>
+           Edit
+         </button>
+         <button type="button" onClick={handleDelete}>
+           Remove
+         </button>
+       </div>}
     </div>
   );
 }
