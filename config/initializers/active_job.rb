8dbb9c7e392e0c7f23bf6380a8b3854ad5532ab7
Inge Jørgensen <inge@elektronaut.no>
2024-02-04 23:14:07 +0100

    Remove dependency on Redis

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-# ActiveJob::Base.queue_adapter = :sidekiq
 ActiveJob::Base.queue_adapter = :solid_queue
--- config/initializers/sidekiq.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/1") }
-end
