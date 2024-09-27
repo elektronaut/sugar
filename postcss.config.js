2209afc4310b212b89c4c855cbfb6fcffc157bdb
Inge Jørgensen <inge@elektronaut.no>
2024-09-27 13:50:21 +0200

    Switch to pnpm

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,16 @@ module.exports = {
     require("postcss-import-ext-glob"),
     require("postcss-import"),
     require("postcss-url")([
-      { filter: "**/webfonts/fa-*",
+      {
+        filter: /webfonts\/fa-/,
         url: "copy",
         basePath: "../../../node_modules/@fortawesome/fontawesome-free/css",
-        assetsPath: "app/assets/builds", }
+        assetsPath: "app/assets/builds"
+      }
     ]),
     require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
-    require("autoprefixer"),
-  ],
-}
+    require("autoprefixer")
+  ]
+};
